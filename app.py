00543f9390354905206e4da80d4822993ad7cae7
Rafael_Espinoza~ <rafael.moreno.espinoza10@gmail.com>
2024-12-10 18:37:44 -0600

    fixed commit

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import pdfplumber
 import os
 import re
 from datetime import datetime
+import uvicorn
 
 app = FastAPI()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
 SKILLS_KEYWORDS = [
     "Python", "JavaScript", "Java", "C++", "HTML", "CSS", "React", "Angular",
